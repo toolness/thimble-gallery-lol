5399632eba6e159d2b6287c43471d836e0180cdd
Atul Varma <varmaa@gmail.com>
2012-07-11 11:42:42 -0400

    app listens on configurable port now

--- app.js
+++ app.js
@@ -82,4 +82,4 @@ app.get('/unique/:start/:count', function(req, res) {
   }));
 });
 app.use(express.static(__dirname + '/static'));
-app.listen(3000);
+app.listen(config.appPort);
--- config.sample.js
+++ config.sample.js
@@ -2,6 +2,7 @@ module.exports = {
   baseThimbleURL: "https://thimble.webmaker.org/p/",
   imageDir: "../images",
   hashDir: "../hashes",
+  appPort: 3000,
   screencapPort: 9012,
   secret: "grGORef",
   auth: {
