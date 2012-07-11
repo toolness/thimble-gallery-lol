398e44732c7e2782a41114936e02b3f77cd026b7
Atul Varma <varmaa@gmail.com>
2012-07-11 18:46:17 -0400

    documented config.sample.js

--- config.sample.js
+++ config.sample.js
@@ -1,13 +1,23 @@
 module.exports = {
+  // Where all the published pages are.
   baseThimbleURL: "https://thimble.webmaker.org/p/",
+  // Where to store screencaps of published pages.
   imageDir: "../images",
+  // The HTTP port that the gallery web app listens on.
   appPort: 3000,
+  // The HTTP port that the PhantomJS screen cap app listens on.
   screencapPort: 9012,
+  // A secret that the PhantomJS app uses for access control, since
+  // it doesn't currently appear to be possible to bind PhantomJS to
+  // 127.0.0.1 only.
   secret: "grGORef",
+  // HTTP basic auth info. Remove this key if you don't want to
+  // protect the gallery with HTTP basic auth.
   auth: {
     username: 'blarg',
     password: 'blarg'
   },
+  // Redis info. Leave port/host at null to use Redis defaults.
   redis: {
     port: null,
     host: null
