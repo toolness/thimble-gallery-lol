9781e997a5afe44fcc57ffd9f2d4d7baf65f52d3
Atul Varma <varmaa@gmail.com>
2012-07-12 09:00:54 -0400

    migrate to mocha and expect.js

---
+++ test/test-config.js
@@ -0,0 +1,22 @@
+var expect = require('expect.js'),
+    fs = require('fs'),
+    PublishedPageTracker = require('../published-page-tracker'),
+    config = require('../config');
+
+describe('config.imageDir', function() {
+  it('should exist on filesystem', function() {
+    expect(fs.existsSync(config.imageDir)).to.be.ok();
+  });
+});
+
+describe('redis', function() {
+  it('should respond to PING', function(done) {
+    var client = PublishedPageTracker.client;
+
+    client.ping(function(err, response) {
+      expect(err).to.be(null);
+      expect(response).to.be("PONG");
+      done();
+    });
+  });
+});
